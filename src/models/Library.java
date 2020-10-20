6fe9c4b481d01689d98d808266d59376c993a185
Drew Gomez <andrew.gomez01@student.csulb.edu>
2020-10-20 00:50:52 -0700

    added the outline of functions for Library, Artist, and Song

--- src/models/Library.java
+++ src/models/Library.java
@@ -1,4 +1,53 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Song> songs;
+
+    public Library()    {
+        // Acquires the database connection
+
+        // Loads the library in from the database
+    }
+
+    public void loadSongsFromDB()   {
+        // Uses the database connection to query the database for the columns in 'Song' on the song DB table
+
+        // Iterates over the result set, and turns each entry in to a song by getting the artist and then making the song object
+
+        // replaces the 'songs' array list with the new array list
+
+        // sort the array list alphabetically by title
+    }
+
+    public void addSongToLibrary(String path)  {
+        // try to add the song the DB
+
+        // first, get the file from the path
+
+        // get the metadata from the song's tags
+
+        // create the artist if needed
+
+        // create the song if needed
+
+        // create a song object out of the metadata, and add the song to the array list of songs
+
+        // sort the array list alphabetically by title
+
+        // if the song cannot be added, throw the corresponding exception
+    }
+
+    public void deleteSongFromLibrary(Song song)  {
+        // query the DB to delete the song by primary key
+
+        // delete the song from the array list that has the songs stored
+
+        // if the song cannot be added, throw the corresponding exception
+    }
+
+    public ArrayList<Song> getLibrary() {
+        return new ArrayList<Song>();
+    }
 }
--- src/models/Song.java
+++ src/models/Song.java
@@ -1,4 +1,39 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Song {
+    private Integer pk;
+    private String title;
+    private Float length;
+    private String genre;
+    private ArrayList<Artist> artists;
+
+    public Song(Integer pk, String title, Float length, String genre, ArrayList<Artist> artists) {
+        this.pk = pk;
+        this.title = title;
+        this.length = length;
+        this.genre = genre;
+        this.artists = artists;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public Float getLength() {
+        return length;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getPk() {
+        return pk;
+    }
+
+    public ArrayList<Artist> getArtists() {
+        return artists;
+    }
 }
