28f2162ea5899973884cb3730b654c6719c540c1
Drew Gomez <andrew.gomez01@student.csulb.edu>
2020-10-20 17:18:07 -0700

    fixed the player class

--- src/models/Player.java
+++ src/models/Player.java
@@ -1,11 +1,17 @@
 package models;
 
+import javazoom.jlgui.basicplayer.BasicPlayer;
+
+import java.io.File;
+import java.util.ArrayList;
+
 enum State  {
     PLAYING,
     PAUSED,
     IDLE
 }
 public class Player {
+    private BasicPlayer basePlayer;
     private State state;
     private Song currentSong;
     private Integer timestamp;
@@ -14,31 +20,126 @@ public class Player {
 
     public Player() {
         state = State.IDLE;
+        basePlayer = new BasicPlayer();
         library = new Library();
-        library.getLibrary();
     }
 
-    private void playNewSong(Song song)  {
-
+    public void play(Song song)  {
+        try {
+            if (currentSong != null) {
+                stop();
+            }
+            basePlayer.open(new File(song.getPath()).getAbsoluteFile());
+            basePlayer.play();
+            currentSong = song;
+            state = State.PLAYING;
+        } catch (Exception e)   {
+            System.out.println(e.getMessage());
+        }
     }
 
-    private void pause()    {
-
+    public void pause()    {
+        try {
+            if (currentSong != null) {
+                basePlayer.pause();
+                state = State.PAUSED;
+            } else  {
+                throw new IllegalStateException("Current song is null but the player attempted to pause the current song");
+            }
+        } catch (Exception e)   {
+            System.out.println(e.getMessage());
+        }
     }
 
-    private void stop() {
-
+    public void play()  {
+        try {
+            if (currentSong != null) {
+                basePlayer.resume();
+                state = State.PLAYING;
+            } else  {
+                throw new IllegalStateException("Current song is null but the player attempted to resume the current song");
+            }
+        } catch (Exception e)   {
+            System.out.println(e.getMessage());
+        }
     }
 
-    private void nextSong() {
-
+    public void stop() {
+        try {
+            basePlayer.stop();
+            currentSong = null;
+            state = State.IDLE;
+        } catch (Exception e)   {
+            System.out.println(e.getMessage());
+        }
     }
 
-    private void previousSong() {
-
+    public void next() {
+        try {
+            ArrayList<Song> songs = library.getSongs();
+            Song newSong = null;
+            if (currentSong == null) {
+                newSong = songs.get(0);
+            } else {
+                for (int i = 0; i < songs.size(); i++) {
+                    if ((songs.get(i).getPk() == currentSong.getPk()) && (i+1 != songs.size())) {
+                        newSong = songs.get(i+1);
+                    } else if (songs.get(i).getPk() == currentSong.getPk() && i+1 == songs.size())  {
+                        newSong = songs.get(0);
+                    }
+                }
+                if (newSong == null)    {
+                    newSong = songs.get(0);
+                }
+            }
+            if (newSong != null) {
+                play(newSong);
+            } else {
+                throw new IllegalStateException("No next song could be selected for some reason");
+            }
+        } catch(Exception e)    {
+            System.out.println(e.getMessage());
+        }
     }
 
-    private void restartSong() {
+    public void previous() {
+        try {
+            ArrayList<Song> songs = library.getSongs();
+            Song newSong = null;
+            if (currentSong == null) {
+                newSong = songs.get(0);
+            } else {
+                for (int i = 0; i < songs.size(); i++) {
+                    if ((songs.get(i).getPk() == currentSong.getPk()) && (i-1 > 0)) {
+                        newSong = songs.get(i-1);
+                    } else if (songs.get(i).getPk() == currentSong.getPk() && i == 0) {
+                        newSong = songs.get(songs.size()-1);
+                    }
+                }
+                if (newSong == null)    {
+                    newSong = songs.get(0);
+                }
+            }
+            if (newSong != null) {
+                play(newSong);
+            } else {
+                throw new IllegalStateException("No next song could be selected for some reason");
+            }
+        } catch(Exception e)    {
+            System.out.println(e.getMessage());
+        }
+    }
 
+    public void restart() {
+        Song current = currentSong;
+        play(current);
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public Library getLibrary() {
+        return library;
     }
 }
--- src/player/Main.java
+++ src/player/Main.java
@@ -1,8 +1,62 @@
 package player;
 
+import javazoom.jlgui.basicplayer.BasicPlayer;
+import models.Library;
+import models.Player;
+
 public class Main {
 
     public static void main(String[] args) {
+        Player player = new Player();
+        Library lib = player.getLibrary();
 
+        lib.addSongToLibrary("11.mp3");
+        lib.addSongToLibrary("21st_Century_Schizoid_Man.mp3");
+        lib.addSongToLibrary("Let_It_Be.mp3");
+        lib.addSongToLibrary("So_What.mp3");
+
+        System.out.println(lib.getSongs());
+
+        player.play(lib.getSongs().get(0));
+
+        for (int i = 0; i < 750000; i++)   {
+            System.out.println("idle");
+        }
+
+        player.pause();
+
+        for (int i = 0; i < 750000; i++)   {
+            System.out.println("idle");
+        }
+
+        player.play();
+
+        for (int i = 0; i < 750000; i++)   {
+            System.out.println("idle");
+        }
+
+        player.restart();
+
+        for (int i = 0; i < 750000; i++)   {
+            System.out.println("idle");
+        }
+
+        player.next();
+
+        for (int i = 0; i < 750000; i++)   {
+            System.out.println("idle");
+        }
+
+        player.next();
+
+        for (int i = 0; i < 750000; i++)   {
+            System.out.println("idle");
+        }
+
+        player.previous();
+
+        for (int i = 0; i < 750000; i++)   {
+            System.out.println("idle");
+        }
     }
 }
